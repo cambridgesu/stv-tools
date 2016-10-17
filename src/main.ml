2ea3d6d43754a074bfd406fcf7d91b5e1c08815b
Martin Keegan <martin@no.ucant.org>
2016-10-18 00:54:44 +0100

    rm old version

--- src/main.ml
+++ src/main.ml
@@ -1,16 +1,3 @@
-(*
-let run () =
-  let b = Blt.create_context () in
-  let blt_context = ref b in
-    (try
-      while true do
-        let line = input_line stdin in
-          blt_context := Blt.handle_line line !blt_context
-      done
-    with
-      End_of_file -> ());
-    Blt.check_consistency !blt_context
-*)
 
 let run () =
   let rec process_blt_file ctx =
