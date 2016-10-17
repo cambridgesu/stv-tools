8bc41a6975271b23a0b132635de209ffb814fae9
Martin Keegan <martin@no.ucant.org>
2016-10-18 00:54:23 +0100

    be tail recursive / don't use state

--- src/main.ml
+++ src/main.ml
@@ -1,4 +1,4 @@
-
+(*
 let run () =
   let b = Blt.create_context () in
   let blt_context = ref b in
@@ -10,5 +10,21 @@ let run () =
     with
       End_of_file -> ());
     Blt.check_consistency !blt_context
+*)
+
+let run () =
+  let rec process_blt_file ctx =
+    let line =
+      try Some (input_line stdin)
+      with End_of_file -> None
+    in
+      match line with
+        | Some l -> let new_ctx = Blt.handle_line l ctx in
+                      process_blt_file new_ctx
+        | None -> ctx
+  in
+    let tally = Blt.create_context () |> process_blt_file in
+      Blt.check_consistency tally;
+      print_endline "DONE"
         
 let _ = run ()
