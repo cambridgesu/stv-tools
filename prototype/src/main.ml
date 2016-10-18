0dca53b58a9386a315e1bf0d436de44f225719d6
Martin Keegan <martin@no.ucant.org>
2016-10-18 18:29:43 +0100

    wrap recursive fn

--- prototype/src/main.ml
+++ prototype/src/main.ml
@@ -1,16 +1,19 @@
 
 let run () =
-  let rec process_blt_file ctx =
-    let line =
-      try Some (input_line stdin)
-      with End_of_file -> None
-    in
-      match line with
+  let process_blt_file input_stream ctx =
+    let rec process_blt_file ctx =
+      let line =
+        try Some (input_line input_stream)
+        with End_of_file -> None
+      in
+        match line with
         | Some l -> let new_ctx = Blt.handle_line l ctx in
                       process_blt_file new_ctx
         | None -> ctx
+    in
+      process_blt_file ctx
   in
-    Blt.create_context () |> process_blt_file |> Blt.check_consistency;
-    print_endline "DONE"
+      Blt.create_context () |> process_blt_file stdin |> Blt.check_consistency;
+      print_endline "DONE"
         
 let _ = run ()
