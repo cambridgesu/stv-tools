8671dfdbd227a733acba6e94fbf36d5572abe558
Martin Keegan <martin@no.ucant.org>
2017-01-06 14:44:51 +0000

    add engine/stage machinery

---
+++ prototype/src/engine.ml
@@ -0,0 +1,48 @@
+
+(*
+
+some of the key invariants are:
+
+  the total number of votes remains constant, over redistributions, exhaustion
+  etc
+
+  the sum of the elected, eliminated and continuing candidates is constant
+
+  the number of elected candidates is strictly less than the total number of
+  seats to be elected (or the loop exits)
+
+  the number of continuing candidates is strictly more than the number of
+  seats remaining unfilled
+
+*)
+open Types
+
+type event =
+  | Elected
+  | Excluded
+  | Distribute_surplus
+  | Distribute_excluded
+
+module Stage : sig
+  type t
+  val initial : tally -> t
+  val next : t -> event -> t
+end =
+struct
+  type t = {
+    x : int
+  }
+
+  let example = {
+    x = 3
+  }
+
+  let initial whatever =
+    example
+
+  let next old_stage event = old_stage
+
+  let consistent stage =
+    true
+
+end
--- prototype/src/main.ml
+++ prototype/src/main.ml
@@ -1,7 +1,7 @@
 
 let run () =
   let tally = Blt.tally_of_blt_stream stdin in
-    ignore tally;
-    print_endline "DONE"
+    print_endline "DONE";
+    Engine.Stage.initial tally
         
 let _ = run ()
