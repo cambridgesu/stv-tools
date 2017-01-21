619201904fccaa576fa9e585027025487326ba7d
Martin Keegan <martin@no.ucant.org>
2017-01-21 21:59:48 +0000

    use immutable lists for long-term checked storage

--- prototype/src/ballot.ml
+++ prototype/src/ballot.ml
@@ -1,7 +1,7 @@
 
 type t = {
   ballot_weight : int;
-  ballot_preferences : int array
+  ballot_preferences : int list
 }
 
 exception Duplicate_prefs
@@ -17,16 +17,16 @@ let rec no_duplicates = function
 
 let check_preferences contest prefs =
   let max_candidate, _ = Contest.get_totals contest in
-    prefs |> Array.iter (
+    prefs |> List.iter (
       fun pref -> if pref > max_candidate then raise (No_such_candidate pref)
           else ()
     );
 
-    prefs |> Array.iter (
+    prefs |> List.iter (
       fun pref -> if pref < 1 then raise (Non_positive_pref pref) else ()
     );
 
-    if no_duplicates (Array.to_list prefs)
+    if no_duplicates prefs
     then ()
     else raise Duplicate_prefs
 
@@ -38,17 +38,17 @@ let create contest weight prefs =
   }
 
 let total_preferences b =
-  Array.length b.ballot_preferences
+  List.length b.ballot_preferences
 
 let dump b =
   Printf.printf "\nBallot weight: %d\n\n" b.ballot_weight;
-  Array.iteri (fun i pref ->
+  List.iteri (fun i pref ->
     Printf.printf "  %d: Option %d\n" (i + 1) pref
   ) b.ballot_preferences
 
 let dump_named names b =
   Printf.printf "\nBallot weight: %d\n\n" b.ballot_weight;
-  Array.iteri (fun i pref ->
+  List.iteri (fun i pref ->
     Printf.printf "  %d: %s\n" (i + 1) (List.nth names (pref - 1))
   ) b.ballot_preferences
   
