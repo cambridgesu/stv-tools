3199a8ed678eb51fce2ab92ba2ffdc1115d66698
Martin Keegan <martin@no.ucant.org>
2017-01-21 17:26:15 +0000

    start to change to tally

--- prototype/src/blt.mli
+++ prototype/src/blt.mli
@@ -1,3 +1,2 @@
-open Types
 
-val tally_of_blt_stream : in_channel -> tally
+val tally_of_blt_stream : in_channel -> Tally.t
--- prototype/src/types.ml
+++
@@ -1,7 +0,0 @@
-
-type tally = {
-  total_candidates : int;
-  total_seats : int;
-  candidate_names : string array;
-  ballots : Ballot.t list;
-}
