928c948fa59e56dcd8dd2994652dd919b893b8de
Martin Keegan <martin@no.ucant.org>
2017-01-21 22:22:58 +0000

    add arg parse for tests

--- prototype/src/test_stv.ml
+++ prototype/src/test_stv.ml
@@ -11,4 +11,11 @@ let suite =
 
 let run () = run_test_tt_main suite
 
-let _ = run ()
+let _ = 
+  let blt_dir = ref "." in
+  let anon s = Printf.printf "Unhandled argument: %s\n" s; exit 2 in
+  let usage = "test_stv [--dir directory]" in
+    Arg.parse [("--dir", Arg.Set_string blt_dir, 
+                "Directory containing BLT test files")] anon usage;
+    run ()
+
