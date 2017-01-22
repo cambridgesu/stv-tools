5e8a45ddb2b6ca2cbc991567c41d10238c8b50bc
Martin Keegan <martin@no.ucant.org>
2017-01-22 17:04:31 +0000

    add copyright header; Closes: #3

--- reference/src/test_stv.ml
+++ reference/src/test_stv.ml
@@ -1,3 +1,14 @@
+(*
+  STV Tools, a reference implementation of STV, by Martin Keegan
+
+  Copyright (C) 2016-2017  Martin Keegan
+
+  This programme is free software; you may redistribute and/or modify
+  it under the terms of the GNU Affero General Public Licence as published by
+  the Free Software Foundation, either version 3 of said Licence, or
+  (at your option) any later version.
+*)
+
 open OUnit
 
 let suite =
