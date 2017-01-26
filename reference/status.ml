32902af87f708438afcd56e2cf2139ef04bbbc86
Martin Keegan <martin@no.ucant.org>
2017-01-26 02:13:33 +0000

    add Status.t

---
+++ reference/status.ml
@@ -0,0 +1,25 @@
+(*
+  STV Tools, a reference implementation of STV, by Martin Keegan
+
+  Copyright (C) 2016-2017  Martin Keegan
+
+  This programme is free software; you may redistribute and/or modify
+  it under the terms of the GNU Affero General Public Licence as published by
+  the Free Software Foundation, either version 3 of said Licence, or
+  (at your option) any later version.
+*)
+
+type t =
+  | Continuing
+  | Elected
+  | Eliminated
+
+let create () = Continuing
+
+let declare_elected = function
+  | Continuing -> Elected
+  | _ -> assert false
+
+let declare_eliminated = function
+  | Continuing -> Eliminated
+  | _ -> assert false
